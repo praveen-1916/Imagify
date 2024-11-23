1c87cf8d4080fe51aacd57955c508cfd1d99673d
praveen-1916 <pkumarbattu@gmail.com>
2024-11-23 21:27:14 +0530

    imageGeneration page issue

--- frontend/src/pages/ImageGeneration.jsx
+++ frontend/src/pages/ImageGeneration.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 import Working from "../components/Working";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
