d4eeafef42c7acb0fef51a9f7aab43b1b7575f03
praveen-1916 <pkumarbattu@gmail.com>
2024-11-26 20:10:02 +0530

    new route testing

--- backend/routes/imageGeneration.js
+++ backend/routes/imageGeneration.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { Generation } from "../controllers/Generationcontroller.js";
+import User from "../models/UserModel.js";
 
 const router = Router();
 
 router.post("/generate", Generation);
+
+router.post("/create", async (req, res) => {
+  try {
+    await User.create(req.body);
+    res.json({ success: true, message: "Created success" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+});
 
 export default router;
