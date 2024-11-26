b3fe29630dab09a8930f952d18b5ed321cce317d
praveen-1916 <pkumarbattu@gmail.com>
2024-11-26 19:25:12 +0530

    db error

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,11 @@
 import { connect } from "mongoose";
 
 const mongoURI = process.env.MONGO_URI;
+console.log(mongoURI);
 
 const connectToMongo = async () => {
   await connect(mongoURI);
+  console.log("connected");
 };
 
 export default connectToMongo;
