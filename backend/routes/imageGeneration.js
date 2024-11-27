34f1d79d55937dfb4585083681eaf58bec28b449
praveen-1916 <pkumarbattu@gmail.com>
2024-11-27 17:06:53 +0530

    commit 4

--- backend/routes/imageGeneration.js
+++ backend/routes/imageGeneration.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { Generation } from "../controllers/Generationcontroller.js";
+import authUser from "../middlewares/authUser.js";
 
 const imageRouter = Router();
 
-imageRouter.post("/generate", Generation);
+imageRouter.post("/generate", authUser, Generation);
 
 export default imageRouter;
